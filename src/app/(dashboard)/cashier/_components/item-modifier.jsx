cdc51367471bb4e23f55634f7afed93e2d0dfb25
marten <arieskenz433@gmail.com>
2026-09-24 19:47:26 +0700

    update item modifiers

--- src/app/(dashboard)/cashier/_components/item-modifier.jsx
+++ src/app/(dashboard)/cashier/_components/item-modifier.jsx
@@ -4,35 +4,38 @@ import { SlidersHorizontal, } from "lucide-react";
 export default function ItemModifier() {
   return (
     <>
+
     <Card>
       <CardHeader>
         <CardTitle><SlidersHorizontal/> Active item Modifiers(Selected/Last Added)</CardTitle>
         
       </CardHeader>
-      <CardContent>
+    <div className="flex items-start gap-2">
+      <CardContent className="p-0">
         <p>DRINK SIZE</p>
         <Button>Small</Button>
         <Button>Medium</Button>
         <Button>Large</Button>
       </CardContent>
-      <CardContent>
+      <CardContent className="p-0">
         <p>MILK SUB</p>
         <Button>Regular</Button>
         <Button>Oat(+$0.80)</Button>
         <Button>Almond</Button>
       </CardContent>
-      <CardContent>
+      <CardContent className="p-0">
         <p>SHOT TUNING</p>
         <Button>Single</Button>
         <Button>Double</Button>
         <Button>Triple ($1.20)</Button>
       </CardContent>
-      <CardContent>
+      <CardContent className="p-0">
         <p>EXTRA ICE</p>
         <Button>No Ice</Button>
         <Button>With Ice</Button>
         <Button>Less Ice</Button>
       </CardContent>
+    </div>
     </Card>
     </>
   );
